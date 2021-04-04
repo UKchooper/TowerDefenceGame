98476815316cde5df682e4bcd405a4e110188010
Carl Hooper <Carl.Hooper@malvernpanalytical.com>
2021-04-04 20:46:27 +0100

    Adding project to git

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.Points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * Speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.1f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if(wavePointIndex >= Waypoints.Points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavePointIndex++;
+        target = Waypoints.Points[wavePointIndex];
+    }
+}
