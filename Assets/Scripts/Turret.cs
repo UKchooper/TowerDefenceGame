8bfef1647ff3364234e8bfb379fe8a9bf376728d
Carl Hooper <Carl.Hooper@malvernpanalytical.com>
2021-04-04 22:15:22 +0100

    Lesson 4: Added turrets

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public float range = 15;
+    public string EnemyTag = "Enemy";
+    public Transform PartToRotate;
+    public float TurnSpeed = 10f;
+
+    private Transform Target;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+        if(Target == null)
+        {
+            return;
+        }
+
+        //Target lock on
+        Vector3 direction = Target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime * TurnSpeed).eulerAngles;
+        PartToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(EnemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            Target = nearestEnemy.transform;
+        }
+        else
+        {
+            Target = null;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform EnemyPrefab;
+    public Transform SpawnPoint;
+    public Text WaveCountdownText;
+
+    public float TimeBetweenWaves = 5.5f;
+    private float countdown = 2f;
+    private int waveIndex = 0;
+    private void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+
+            countdown = TimeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        WaveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(EnemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
